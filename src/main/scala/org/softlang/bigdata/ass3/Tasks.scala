6730d89cc0a2b5cf6b71dec8ba1ba11e8ee47bba
sanoj3008 <jonas.schell@icloud.com>
2022-05-20 16:04:51 +0200

    task file completed

--- src/main/scala/org/softlang/bigdata/ass3/Tasks.scala
+++ src/main/scala/org/softlang/bigdata/ass3/Tasks.scala
@@ -9,25 +9,25 @@ object Tasks {
     assert(task1(Seq(1, 2, 3)) == Seq("odd", "even", "odd"))
 
     // Task 2:
-//    assert(task2(Seq("ClassA.scala", "ClassB.java", "readme.txt")) == Seq("ClassB.java"))
+    assert(task2(Seq("ClassA.scala", "ClassB.java", "readme.txt")) == Seq("ClassB.java"))
 
     // Task 3:
-//    assert(task3(Seq(1, 2, 3)) == 1 * 2 * 3)
+    assert(task3(Seq(1, 2, 3)) == 1 * 2 * 3)
 
     // Task 4:
-//    assert(task4(Seq("a", "b", "c")) == "abc")
+    assert(task4(Seq("a", "b", "c")) == "abc")
 
     // Task 5:
-//    assert(task5(Seq("m1", "m2", "m3"), "m1"))
-//    assert(!task5(Seq("m1", "m2", "m3"), "m4"))
+    assert(task5(Seq("m1", "m2", "m3"), "m1"))
+    assert(!task5(Seq("m1", "m2", "m3"), "m4"))
 
     // Task 6:
-//    assert(task6(Seq(("k1", "v1"), ("k1", "v2"), ("k2", "v1"))) == Map("k1" -> Seq("v1", "v2"), "k2" -> Seq("v1")))
+    assert(task6(Seq(("k1", "v1"), ("k1", "v2"), ("k2", "v1"))) == Map("k1" -> Seq("v1", "v2"), "k2" -> Seq("v1")))
 
     // Task 7:
     val l = Seq(("k1", "v1"), ("k2", "v2"))
     val r = Seq(("k2", "vv2"), ("k3", "vv2"))
-//    assert(task7(l, r) == Seq(("k2", "v2", "vv2")))
+    assert(task7(l, r) == Seq(("k2", "v2", "vv2")))
   }
 
   /**
@@ -41,22 +41,24 @@ object Tasks {
   /**
    * Filter a list of path names for ".java" files.
    */
-  def task2(seq: Seq[String]): Seq[String] = ???
+  def task2(seq: Seq[String]): Seq[String] = seq.filter(e => e.takeRight(5) == ".java")
 
   /**
    * Form the product of all numbers contained in the collection.
    */
-  def task3(seq: Seq[Int]): Int = ???
+  def task3(seq: Seq[Int]): Int = seq.reduce((x,y) => x*y)
+  def task3Improved(seq: Seq[Int]): Int = seq.product
 
   /**
    * Concat the individual strings of this collection forming a single string.
    */
-  def task4(seq: Seq[String]): String = ???
+  def task4(seq: Seq[String]): String = seq.reduce((a,b) => a+b)
 
   /**
    * Check if x is a member (contained) in the sequence.
    */
-  def task5(seq: Seq[String], x: String): Boolean = ???
+  def task5(seq: Seq[String], x: String): Boolean = seq.map(e => e == x).reduce((x,y) => x || y)
+  def task5Improved(seq: Seq[String], x: String): Boolean = seq.contains(x)
 
   /**
    * (Optional) Implement grouping by a key, without using the "groupBy" method provided on the collection,
@@ -64,13 +66,19 @@ object Tasks {
    * The first entry of the tuples is the key that you should use for grouping.
    * Optional: This is an advanced assignment, you may get additional points.
    */
-  def task6(seq: Seq[(String, String)]): Map[String, Seq[String]] = ???
+  def task6(seq: Seq[(String, String)]): Map[String, Seq[String]] = seq.map(elem => Map(elem._1 -> Seq(elem._2)))
+    .fold(Map.empty[String, Seq[String]])((result, elem) => {
+      val (key, value) = elem.head
+      result.updated(key, result.getOrElse(key, Seq()).appended(value.head))
+    })
 
   /**
    * (Optional) Join the two sequences by the key (first entry of the tuple). Produce a 3-tuple containing
    * the results (key, left, right).
    * Optional: This is an advanced assignment, you may get additional points.
    */
-  def task7(l: Seq[(String, String)], r: Seq[(String, String)]): Seq[(String, String, String)] = ???
-
+  def task7(l: Seq[(String, String)], r: Seq[(String, String)]): Seq[(String, String, String)] = task6(l.concat(r))
+    .filter(elem => elem._2.length == 2)
+    .toSeq
+    .map(elem => (elem._1, elem._2.head, elem._2(1)))
 }
